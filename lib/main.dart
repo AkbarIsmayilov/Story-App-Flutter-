bcb54ab8c30fe9368bce19d78f8d25613145351b
AkbarIsmayilov <akbarismayilov1@gmail.com>
2021-03-05 21:06:44 +0400

    git initiliazed

---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  int storyNumber = 0;
+
+  void setStoryNumber(int newStoryNum) {
+    setState(() {
+      storyNumber = newStoryNum;
+    });
+  }
+
+  void nextStory(int choiceNumber) {
+    print('next story ');
+    switch (storyNumber) {
+      case 0:
+        if (choiceNumber == 1) {
+          setStoryNumber(2);
+        } else {
+          setStoryNumber(1);
+        }
+        break;
+      case 1:
+        if (choiceNumber == 1) {
+          setStoryNumber(2);
+        } else {
+          setStoryNumber(3);
+        }
+        break;
+      case 2:
+        if (choiceNumber == 1) {
+          setStoryNumber(5);
+        } else {
+          setStoryNumber(4);
+        }
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                    storyBrain.getStory(storyNumber),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    nextStory(1);
+                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
+                    storyBrain.getChoice1(storyNumber),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {
+                    //Choice 2 made by user.
+                    nextStory(2);
+                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                  },
+                  color: Colors.blue,
+                  child: Text(
+                    //TODO: Step 14 - Use the storyBrain to get the text for choice 1.
+                    storyBrain.getChoice2(storyNumber),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//TODO: Step 22 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
